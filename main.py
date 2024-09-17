0b6021f560e4e1ab5c9e2a6f891756455ead66d5
Alan Ramos López <118926925+ARandomGuyZero@users.noreply.github.com>
2024-09-17 12:13:19 -0700

    Update main.py
    Updated code to address an keyerror issue when the user didn't use characters from the alphabet

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 NATO Alphabet
 
 Author: Alan
-Date: September 13th
+Date: September 13th 2024
+Update: September 17th 2024
 
 This script prints a list of words of the phonetic alphabet based on the user's input.
 """
@@ -15,11 +16,22 @@ nato_alphabet_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # Create new dictionary using the letter and the code
 new_dict = {row.letter:row.code for (index, row) in nato_alphabet_data.iterrows()}
 
-# Ask user for a word
-word = input("Input your name:\n").upper()
+def generate_nato_word_list():
 
-# Create new list using each letter of the word for a new dictionary
-word_list = [new_dict[letter] for letter in word]
+    # Ask user for a word
+    word = input("Input your name:\n").upper()
 
-# Print new list of words
-print(word_list)
+    try:
+
+        # Create new list using each letter of the word for a new dictionary
+        word_list = [new_dict[letter] for letter in word]
+
+        # Prints the list of words
+        print(word_list)
+
+    except KeyError:
+
+        print("Sorry, only letters in the alphabet, please.")
+        generate_nato_word_list()
+
+generate_nato_word_list()
